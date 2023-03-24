7b8417579d4538c256f8bb7b930e613d8794efe1
Didaka19 <124637285+Didaka19@users.noreply.github.com>
2023-03-24 14:39:19 +0000

    Add files via upload

---
+++ pa-chat-room-main/client/src/main/java/Chat_Frame.java
@@ -0,0 +1,141 @@
+package client.src.main.java;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Chat_Frame extends JFrame implements ActionListener {
+    private String client_name;  //nome de cliente
+    private JButton btnSend;
+    private JButton btnExit;
+    private JTextArea area_messegers;
+    private JPanel panel;
+    private JTextField user_msg;
+    private JTextField name_input;
+    private JLabel lbl_user_msg;
+    private JLabel buf_msg;//historico de chat
+    private boolean btnSend_isClicked;
+    private boolean btnExit_isClicked;
+
+    public Chat_Frame() {
+
+        btnSend_isClicked = false;
+        btnExit_isClicked = false;
+
+        name_input = new JTextField("Escreve o nome");
+        Object[] texts = {name_input};
+
+        //JFrame chat = new JFrame("chat");
+
+        JOptionPane.showMessageDialog(null,texts); //talvez addicionar
+
+        panel =  new JPanel();
+        panel.setBackground(Color.LIGHT_GRAY);
+
+        buf_msg = new JLabel("Historico");
+
+        btnExit = new JButton("Sair");
+        btnExit.addActionListener((ActionListener) this);
+
+        btnSend = new JButton("Enviar");
+        btnSend.addActionListener(this);
+
+        user_msg = new JTextField(50);
+        user_msg.setBorder(BorderFactory.createEtchedBorder(Color.ORANGE, Color.ORANGE));
+
+        lbl_user_msg=new JLabel("Sua Mensagem");
+
+        client_name=name_input.getName();
+        area_messegers =new JTextArea(30,50);
+        area_messegers.setEditable(false);
+        JScrollPane scroll = new JScrollPane(area_messegers);
+        area_messegers.setBackground(Color.WHITE);
+        area_messegers.setLineWrap(true);
+        area_messegers.setBorder(BorderFactory.createEtchedBorder(Color.GREEN,Color.GREEN));
+
+
+
+        panel.add(buf_msg);
+        panel.add(scroll);
+        panel.add(lbl_user_msg);
+        panel.add(user_msg);
+        panel.add(btnSend);
+        panel.add(btnExit);
+
+        setSize(600,650);
+        setContentPane(panel);
+        setTitle(name_input.getText());
+        setClient_name(name_input.getText());
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+
+    public void setClient_name(String client_name) {
+        this.client_name = client_name;
+    }
+
+
+    public JTextArea get_area() {
+        return area_messegers;
+    }
+    public JTextField getUser_msg() {
+        return user_msg;
+    }
+    public String getClient_name() {
+        return client_name;
+    }
+
+    public JButton getBtnSend() {
+        return btnSend;
+    }
+
+    public JButton getBtnExit() {
+        return btnExit;
+    }
+
+    public void Sair(ClientThread client) {
+        client.CloseThread();
+    }
+
+    public boolean isBtnSend_isClicked() {
+        return btnSend_isClicked;
+    }
+
+    public void setBtnSend_isClicked(boolean btnSend_isClicked) {
+        this.btnSend_isClicked = btnSend_isClicked;
+    }
+
+    public boolean isBtnExit_isClicked() {
+        return btnExit_isClicked;
+    }
+
+    public void setBtnExit_isClicked(boolean btnExit_isClicked) {
+        this.btnExit_isClicked = btnExit_isClicked;
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("Enviar")) {
+            btnSend_isClicked = true;
+        } else if (e.getActionCommand().equals("Sair"))
+            btnExit_isClicked = true;
+    }
+
+    /*
+    @Override
+    public void keyTyped(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_ENTER){
+            btnSend_isClicked = true;
+        }
+    }
+    */
+
+
+
+}
--- pa-chat-room-main/client/src/main/java/ClientThread.java
+++ pa-chat-room-main/client/src/main/java/ClientThread.java
@@ -1,117 +1,131 @@
+package client.src.main.java;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.net.Socket;
-import java.util.ArrayList;
+import java.io.Writer;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 
 public class ClientThread extends Thread {
-    private String ClientName;
+
     private final int port;
     private final int id;
-    private final int freq;
-    private DataOutputStream out;
-    private BufferedReader in;
-    private Socket socket;
+    //private final int freq;
+    private Writer ouw;
+   private OutputStream out; //Enviar message
+    private BufferedReader in; //Ler message
+    private Socket socket;//Connection
+    private BufferedWriter bfw;
+    private String send_message;
+    private String receive_message;
+    private String ClientName;
+    private Chat_Frame chat;
+    private JFrame ver;
+    private JButton ok;
 
-    public ClientThread ( int port , int id , int freq ) {
+    private JTextArea text;
+
+    public ClientThread ( int port , int id) {
+
         this.port = port;
         this.id = id;
-        this.freq = freq;
-        this.ClientName = in.readLine();
-        broadcastMsg("SERVER:" + this.ClientName + "entra em chat");
+        chat = new Chat_Frame();
+
     }
-
-
-    private String getName() {
-        return ClientName;
-    }
-
-
-
-
-
-
 
 
 
     public void run ( ) {
-        //try {
-        int i = 0;
-        while ( true ) {
-            System.out.println ( "Sending Data" );
-            try {
-                // if(sem.tryAcquire(1, TimeUnit.SECONDS)) {
-                socket = new Socket ( "localhost" , port );
-                out = new DataOutputStream ( socket.getOutputStream ( ) );
-                in = new BufferedReader ( new InputStreamReader ( socket.getInputStream ( ) ) );
-                out.writeUTF ( "My message number " + i + " to the server " + "I'm " + id );
-                String response;
-                response = in.readLine ( );
-                System.out.println ( "From Server " + response );
-                out.flush ( );
-                socket.close ( );
-                sleep ( freq );
-                i++;
-            } catch ( IOException | InterruptedException e ) {
-                e.printStackTrace ( );
-            }
-        }
-
-        String MsgFromClient;
-        while (socket.isConnected()) {   //enviar mensagens
-            try {
-                MsgFromClient = in.readLine();
-                broadcastMsg(MsgFromClient);
-            } catch (IOException e) {
-                CloseThread(socket,in,out);
-                break;
-            }
-        }
-
-    }
-
-
-    //Enviar mensagens
-    public void broadcastMsg(String message) {    //broadcast
-        for (ClientThread Client : clientThread) {
-            try {
-                if (!Client.getName.equals(this.getName())) {
-                    Client.out.write(message);
-                    Client.out.newLine();
-                    Client.out.flush();
-                }
-
-            } catch (IOException e) {
-                CloseThread(this.socket, this.in,this,out);
-            }
-
-        }
-
-    }
-
-
-    public void removeClientThread() {        //alguem saiu 
-        ClientThreads.remove(this);  //array com clients
-        broadcastMsg("SERVER:" + this.ClientName + "saiu de chat");
-    }
-
-
-    public void CloseThread(Socket socket, BufferedReader bufferedReader,  DataOutputStream dataOutputStream) {
-        removeClientThread();
         try {
-            if(bufferedReader != NULL) {
-                bufferedReader.close();
+            // if(sem.tryAcquire(1, TimeUnit.SECONDS)) {
+            socket = new Socket ( "localhost" , port );
+            out = socket.getOutputStream(); //conection
+            ouw = new OutputStreamWriter(out);
+            bfw = new BufferedWriter(ouw);
+            bfw.flush ( );
+            System.out.println ( "Sending Data" );
+            listen_to_server(chat);
+            boolean i = true;
+            while (i) {
+                Action(chat);
+                try {
+                    sleep(10);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
-            if(dataOutputStream != NULL) {
-                dataOutputStream.close();
+
+        } catch ( IOException e ) {
+            e.printStackTrace ( );
+        }
+
+    }
+
+
+
+    public void enviar_to_server(String msg, Chat_Frame chat) {
+        try
+        {
+            bfw.write(msg+"\r\n");
+            chat.get_area().append(chat.getClient_name()+": " + chat.getUser_msg().getText() + "\r\n");
+            bfw.flush();
+            chat.getUser_msg().setText("");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public void listen_to_server(Chat_Frame chat) {
+        try {
+            in = new BufferedReader ( new InputStreamReader ( socket.getInputStream ( ) ) );
+            String msg = "";
+            System.out.println("Listen");
+            if(in.ready()) {
+                msg = in.readLine();
             }
-            if(socket != NULL) {
-                socket.close();
+            chat.get_area().append(msg+"\r\n");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public void CloseThread() {
+        try {
+            if(this.in != null) {
+                this.in.close();
             }
+            if(this.out != null) {
+                this.out.close();
+            }
+            if(this.socket != null) {
+                this.socket.close();
+            }
+            chat.setVisible(false);
+            chat.dispose();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void Action(Chat_Frame chat){
+        try {
+            if(chat.isBtnSend_isClicked()) {
+                enviar_to_server(chat.getUser_msg().getText(), chat);
+                chat.setBtnSend_isClicked(false);
+            } else if(chat.isBtnExit_isClicked()){
+                CloseThread();
+                chat.setBtnExit_isClicked(false);
+            }
+            } catch (Exception ex) {
+            throw new RuntimeException(ex);
         }
     }
 
